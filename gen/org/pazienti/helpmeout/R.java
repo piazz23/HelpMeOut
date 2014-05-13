1937ced58bbd2a33e4c3d0529e1675582f898ef3
Salvatore Piazzolla <piazz23@gmail.com>
2014-05-13 22:38:46 +0200

    fix empty background

--- gen/org/pazienti/helpmeout/R.java
+++ gen/org/pazienti/helpmeout/R.java
@@ -10,6 +10,9 @@ package org.pazienti.helpmeout;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int light_blue=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,33 +21,35 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int publisher_height_dimension=0x7f040003;
-        public static final int publisher_width_dimension=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int publisher_height_dimension=0x7f050003;
+        public static final int publisher_width_dimension=0x7f050002;
     }
     public static final class drawable {
         public static final int bg_pazienti=0x7f020000;
-        public static final int btn_orange_disable=0x7f020001;
-        public static final int btn_orange_normal=0x7f020002;
-        public static final int btn_orange_pressed=0x7f020003;
-        public static final int btn_pazienti_orange=0x7f020004;
-        public static final int et_pazienti_white=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
+        public static final int bg_pazienti_color=0x7f020001;
+        public static final int btn_orange_disable=0x7f020002;
+        public static final int btn_orange_normal=0x7f020003;
+        public static final int btn_orange_pressed=0x7f020004;
+        public static final int btn_pazienti_orange=0x7f020005;
+        public static final int et_pazienti_white=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int cmd_login=0x7f080002;
-        public static final int fragment_video=0x7f080005;
-        public static final int login_layout=0x7f080000;
-        public static final int login_layout_background=0x7f080001;
-        public static final int pager=0x7f080004;
-        public static final int progressBar1=0x7f080009;
-        public static final int publisher_view=0x7f080007;
-        public static final int subscriber_view=0x7f080006;
-        public static final int txt_login_code=0x7f080003;
-        public static final int txt_video_loading=0x7f08000a;
-        public static final int video_loading_view=0x7f080008;
+        public static final int action_settings=0x7f09000c;
+        public static final int backgroud=0x7f090009;
+        public static final int cmd_login=0x7f090002;
+        public static final int fragment_video=0x7f090005;
+        public static final int login_layout=0x7f090000;
+        public static final int login_layout_background=0x7f090001;
+        public static final int pager=0x7f090004;
+        public static final int progressBar1=0x7f09000a;
+        public static final int publisher_view=0x7f090007;
+        public static final int subscriber_view=0x7f090006;
+        public static final int txt_login_code=0x7f090003;
+        public static final int txt_video_loading=0x7f09000b;
+        public static final int video_loading_view=0x7f090008;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -53,19 +58,19 @@ public final class R {
         public static final int fragment_video=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050007;
-        public static final int login_button_label=0x7f050008;
-        public static final int login_code_text_hint=0x7f050009;
-        public static final int title_activity_login=0x7f050006;
-        public static final int title_section1=0x7f050001;
-        public static final int title_section2=0x7f050002;
-        public static final int title_section3=0x7f050003;
-        public static final int txt_video_loading_label=0x7f050005;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060007;
+        public static final int login_button_label=0x7f060008;
+        public static final int login_code_text_hint=0x7f060009;
+        public static final int title_activity_login=0x7f060006;
+        public static final int title_section1=0x7f060001;
+        public static final int title_section2=0x7f060002;
+        public static final int title_section3=0x7f060003;
+        public static final int txt_video_loading_label=0x7f060005;
     }
     public static final class style {
         /** 
@@ -89,12 +94,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int PazientiOrangeButton=0x7f060002;
-        public static final int PazientiWhiteEditText=0x7f060003;
+        public static final int AppTheme=0x7f070001;
+        public static final int PazientiOrangeButton=0x7f070002;
+        public static final int PazientiWhiteEditText=0x7f070003;
     }
 }
